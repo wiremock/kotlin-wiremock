315471f3742416cb07f4f65402a883797dc8de7e
Marcin Ziolo <marcin.ziolo@tesco.com>
2020-07-12 17:46:07 +0200

    releasing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import nu.studer.gradle.credentials.domain.CredentialsContainer
 
 plugins {
     id("org.jetbrains.kotlin.jvm") version "1.3.61"
@@ -5,6 +6,9 @@ plugins {
     id("io.gitlab.arturbosch.detekt") version "1.1.1"
     jacoco
     `java-library`
+    signing
+    `maven-publish`
+    id("nu.studer.credentials") version "1.0.7"
 }
 
 group = "com.marcinziolo"
@@ -35,14 +39,6 @@ dependencies {
     testImplementation("io.mockk:mockk:1.10.0")
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-        }
-    }
-}
-
 tasks.test {
     useJUnitPlatform()
     finalizedBy(tasks.jacocoTestReport)
@@ -55,4 +51,75 @@ tasks.jacocoTestReport {
         xml.isEnabled = true
         csv.isEnabled = false
     }
+}
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+val credentials: CredentialsContainer by project.extra
+val ossrhUser = (System.getProperty("ossrhUser") ?: credentials.getProperty("ossrhUser") ?: "-") as String
+val ossrhPassword = (System.getProperty("ossrhPassword") ?: credentials.getProperty("ossrhPassword") ?: "-") as String
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "kotlin-wiremock"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Kotlin Wiremock")
+                description.set("Handy Kotlin DSL for Wiremock stubbing")
+                url.set("https://github.com/marcinziolo/kotlin-wiremock")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("marcinziolo")
+                        name.set("Marcin Zioło")
+                        email.set("martin.ziolo@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com:marcinziolo/kotlin-wiremock.git")
+                    developerConnection.set("scm:git:ssh://github.com:marcinziolo/kotlin-wiremock.git")
+                    url.set("https://github.com/marcinziolo/kotlin-wiremock")
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri("https://oss.sonatype.org/service/local/staging/deploy/maven2")
+            val snapshotsRepoUrl = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            credentials {
+                username = ossrhUser
+                password = ossrhPassword
+            }
+        }
+    }
+}
+
+tasks.javadoc {
+    if (JavaVersion.current().isJava9Compatible) {
+        (options as StandardJavadocDocletOptions).addBooleanOption("html5", true)
+    }
+}
+
+signing {
+    sign(publishing.publications["mavenJava"])
 }
